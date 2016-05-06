d44b245a85ab9402788e3fe8e302b2ff162fb080
Ron Friedman <ron852719@icloud.com>
2016-05-06 16:44:02 -0700

    Initial Commit
    iOS app for Newawe.

---
+++ Newawe/Newawe/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Newawe
+//
+//  Created by Ron Friedman on 2016-05-05.
+//  Copyright © 2016 Newawe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var newAweWebView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "http://newawelive.herohamp.xyz")
+        let request = NSURLRequest(URL: url!)
+        self.newAweWebView.loadRequest(request)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
